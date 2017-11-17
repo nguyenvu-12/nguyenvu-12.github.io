e7b90727d94e1c6b0b3fb1b71f37074e9c7f4b3f
nguyenvu-12 <31845612+nguyenvu-12@users.noreply.github.com>
2017-11-17 21:56:33 +0700

    ga
    ga

---
+++ project2/js/script.js
@@ -0,0 +1,61 @@
+$('#hien-mk').on('click',function () {
+var passInput=$('#password-signup').attr('type');			
+	if (passInput == 'password') {
+		$('#password-signup').attr('type', 'text');
+	}
+	if (passInput == 'text') {
+		$('#password-signup').attr('type', 'password');
+	}
+});
+
+$('.taotaikhoan').on('click', function(){
+	$('#sign-in').modal('hide');
+	setTimeout(function(){
+		$('#sign-up').modal('show');}, 500);	
+});
+
+
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+
+
+
+
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
